ece3415c30cd5ae852f66fe9c294c357ac6a1c0f
zyunki <iwaju2160@gmail.com>
2024-05-07 21:47:38 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,13 @@
+function post (){
+  const form = document.getElementById("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+ };
+ 
+ window.addEventListener('turbo:load', post);
