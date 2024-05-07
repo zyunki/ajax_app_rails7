74b39c5fab6ab0465d8d678c0eb5a8bf5966cbaa
zyunki <iwaju2160@gmail.com>
2024-05-07 19:11:06 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,13 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.order(id: "DESC")
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+    redirect_to action: :index
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  root to: 'posts#index'  
+  post 'posts', to: 'posts#create'
+end
